730fdcc1121a4b814110438ca8c7810b56d459c6
jhonathanfeijo <jhonathanfeijomoraes@gmail.com>
2023-04-30 21:06:04 -0400

    "robotron 2000"

---
+++ js/main.js
@@ -0,0 +1,61 @@
+const robotron = document.querySelector("#robotron");
+const controles = document.querySelectorAll("[data-controle]");
+const estatisticas = document.querySelectorAll("[data-estatistica]");
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("[data-contador]");
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
+    }
+    else if (operacao === "+") {
+        peca.value = parseInt(peca.value) + 1;
+    }
+}
+
+function atualizaEstatistica(peca){
+    console.log(pecas[peca]);
+    estatisticas.forEach( (estatistica)=>{
+        estatistica.textContent = parseInt(estatistica.textContent) + pecas[peca][estatistica.dataset.estatistica];
+    })
+}
+
+controles.forEach((controle) => {
+    controle.addEventListener("click", (controle) =>{
+        manipulaDados(controle.target.dataset.controle, controle.target.parentNode);
+        atualizaEstatistica(controle.target.dataset.peca);
+    })
+});
+
